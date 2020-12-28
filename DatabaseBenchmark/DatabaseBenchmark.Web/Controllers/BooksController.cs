0419d6683220cc090cd9652db488fed0c1ff85b6
sa-mamun <samamun009@gmail.com>
2020-12-28 18:09:17 +0600

    Validation and Logger Configuration Added

--- DatabaseBenchmark/DatabaseBenchmark.Web/Controllers/BooksController.cs
+++ DatabaseBenchmark/DatabaseBenchmark.Web/Controllers/BooksController.cs
@@ -1,5 +1,6 @@
 ﻿using DatabaseBenchmark.Core;
 using DatabaseBenchmark.Web.Models;
+using log4net;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
@@ -13,6 +14,10 @@ namespace DatabaseBenchmark.Web.Controllers
 {
     public class BooksController : Controller
     {
+        #region Logger
+        private static readonly ILog _logger = LogManager.GetLogger(typeof(BooksController));
+        #endregion
+
         // GET: Book
         public ActionResult Index()
         {
@@ -60,8 +65,18 @@ namespace DatabaseBenchmark.Web.Controllers
                 {
                     var getValueVM = new GetValueVM();
 
+                    string startTime = DateTime.Now.ToString("h:mm:ss tt");
+
                     var result = getValueVM.GetBookValue(model.BookKey);
+
+                    string endTime = DateTime.Now.ToString("h:mm:ss tt");
+                    // Calculating TimeSpan
+                    TimeSpan duration = DateTime.Parse(endTime).Subtract(DateTime.Parse(startTime));
+
                     var json = JsonConvert.DeserializeObject<BookJso>(result.BookValue);
+
+                    _logger.Info(duration.ToString());
+
                     return Json(json);
                 }
                 catch(Exception ex)
